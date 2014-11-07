e71d897762f116c4b1a996a094024c79e6e5dd51
root <root@localhost.localdomain>
2014-11-07 19:48:33 +0100

    Changes to the feature extraction Now the code can use a variable number of eigenvectors Todo: 1. Split into multiple files 2. Ammortize feature extraction

--- feature_extraction/eigen_faces.py
+++ feature_extraction/eigen_faces.py
@@ -3,10 +3,21 @@ import csv
 import numpy
 import scipy
 import random
+import pylab as pl
+
 from scipy import linalg
 from scipy.misc import toimage
+from sklearn.cross_validation import train_test_split
+from sklearn.datasets import fetch_lfw_people
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.decomposition import RandomizedPCA
+from sklearn import svm
+from sklearn.svm import SVC
 
 csv_file = open('/root/CS229-project/Datasets/train.csv', 'r');
+#csv_file = open('/root/CS229-project/Datasets/train_small.csv', 'r');
 
 reader = csv.reader(csv_file)
 
@@ -49,24 +60,30 @@ random_face_array = numpy.array(random_face_list)
 face_mean = numpy.mean(random_face_array, 0)
 faces_adjusted = random_face_array - face_mean
 
-faces_U, faces_S, faces_V = linalg.svd(faces_adjusted.transpose(), full_matrices=False)
+#faces_U, faces_S, faces_V = linalg.svd(faces_adjusted.transpose(), full_matrices=False)
+#eig_val, eig_vect = linalg.eig(faces_adjusted)
+pca = RandomizedPCA(n_components = 20, whiten=True).fit(faces_adjusted)
+print "Calculating eigen faces done"	
+
+print len(pca.components_)
+#toimage(numpy.reshape((pca.components_[1]), (48, 48))).show()
 
 feature_vector_list = list()
 for i in range(0, len(X_list)):
 	feature_vector_list.append(list())
-print "Calculating eigen faces done"	
 
-for i in range(0, len(X_list)):
-	for U in faces_U.transpose():
-		feature_vector_list[i].append(numpy.dot(X_list[i], U))
-
+feature_vector_list = pca.transform(X_list)
 print "Mapped inputs to features"
+
 feature_file = open('train.feat', 'w')
 for X in feature_vector_list:
 	 feature_file.write("%s\n" % X)
 
 feature_file.close()
 
-print "Feaute extraction done"
-#toimage(numpy.reshape((faces_U.transpose()[1]), (48, 48))).show()
-#toimage(numpy.reshape(X_list[i], (48, 48))).show()
+expr_classifier = svm.SVC()
+expr_classifier.fit(feature_vector_list, Y_list)
+print("Number of support vectors")
+print(expr_classifier.n_support_)
+
+
