974aca6e2f006f35ce4a0ab2f7825624b967bf82
root <root@localhost.localdomain>
2014-11-19 00:19:19 -0800

    gabor feature extraction along with dumping out gabor features

--- feature_extraction/gabor.py
+++ feature_extraction/gabor.py
@@ -48,20 +48,16 @@ def gabor():
 	    frequency = 1 / (2 * pow(math.sqrt(2), f) )
 	    for theta in range(0, 8):
 	        theta = (np.pi * theta) / 8.0
-                #print "frequency", frequency, "theta", theta
-	    #for sigma in range(0, 8):
-            #    sigma = 1.0 + (sigma * 0.625)
-		# TODO: check effect of frequency on kernel size
-	        #for frequency in (0.1, 0.2):
 	        kernel = np.real(gabor_kernel(frequency, theta=theta, sigma_x=sigma, sigma_y=sigma))
 	        kernels.append(kernel)
 
-	fileHandle = open('../Datasets/train_head.csv', 'r')
+	fileHandle = open('../Datasets/train.csv', 'r')
 	reader = csv.reader(fileHandle)
 
 	#gabor_features = list()
 	grid_img = Image.new('P', (432, 384))
 
+	gab_file = open('gabor_feats.csv', 'w')
 	for row in reader:
 		labelStr, featureStr = row
 		label = int(labelStr)
@@ -70,46 +66,20 @@ def gabor():
 		trn_2D = np.reshape(np.array(features), (48, 48))
 		
 		gab_images =  compute_feats(trn_2D, kernels)
-		print "len_gab_img", len(gab_images)
-		print "len_kernels", len(kernels)
-		#print gab_images[0]
-		#toimage(gab_images[1]).show()
-		#toimage(trn_2D).show()
-
-		break
-		#avg_gab_image = np.zeros_like(gab_images[0])
-		
-		#for ittrImg in range(0, len(gab_images)):
-		#	avg_gab_image = avg_gab_image + gab_images[ittrImg]		
-		#
-		#avg_gab_image = avg_gab_image / len(gab_images)
-		#avg_gab_array = avg_gab_image.ravel()
-
-		#gabor_features.append(avg_gab_array)
-		#print len(gabor_features)
-
-	#	with open("gabor_feats.csv", "a") as f:
-	#		writer = csv.writer(f)
-	#		writer.writerow(avg_gab_array)
-	for i in range(0, 9):
-		for j in range(0, 8):
-			grid_img.paste(toimage(gab_images[i*8 + j]), (i * 48, j * 48))
-	grid_img.show()
+		#with open("gabor_feats.csv", "a") as f:
+		gab_file.write(labelStr + ',')
+		for img in gab_images:
+			img_array = img.ravel()
+			for pixel in img_array:
+				gab_file.write(str(pixel) + ' ')
+		gab_file.write('\n')
+	gab_file.close()	
+	#for i in range(0, 9):
+#		for j in range(0, 8):
+#			grid_img.paste(toimage(gab_images[i*8 + j]), (i * 48, j * 48))
+#	grid_img.show()
 	fileHandle.close()
 	
 	print "Finished extracting gabor features"
-
-#	print "Going for dimensionality reduction using PCA"
-#	pca = RandomizedPCA(n_components = 65).fit(np.array(gabor_features))
-	#toimage(avg_gab_image).show()
-	#toimage(gab_images[0]).show()
-	
-#	pcaFeatureVectors = eig.mapRawFeaturesToPCAFeatures( labelledTrainData, pca )
-
-#	return(pca, pcaFeatureVectors)
-#eig.writeFeatureVectorsToFile('train.feat', pcaFeatureVectors)
-#classifier = eig.trainSVM(pcaFeatureVectors, labelledTrainData.labels)
-#pcaTestVectors = mapRawFeaturesToPCAFeatures( labelledTestData, pca )
-#testSVM(pcaTestVectors, labelledTestData.labels, classifier)
 
 gabor()
