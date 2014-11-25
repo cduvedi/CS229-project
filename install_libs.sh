8226d381debc852e5d1e72e1221fdb8b1e97a50c
Chinmay Narendra Duvedi <cduvedi@corn29.stanford.edu>
2014-11-24 17:40:12 -0800

    Fixing the installation script

--- install_libs.sh
+++ install_libs.sh
@@ -1,16 +1,16 @@
 #!/bin/tcsh
 
-setenv PYTHONPATH ${HOME}
+setenv PYTHONPATH ${HOME}:${HOME}/lib/python2.7/site-packages/
 echo $PYTHONPATH
 
 # Install networkx 1.9
-cd ..
 wget https://pypi.python.org/packages/source/n/networkx/networkx-1.9.1.tar.gz#md5=a2d9ee8427c5636426f319968e0af9f2
 tar -xzvf networkx-1.9.1.tar.gz
 cd networkx-1.9.1
 python setup.py install --prefix=${HOME}
 
 
+cd ..
 # Install Cython 0.21
 wget http://cython.org/release/Cython-0.21.1.tar.gz
 tar -xzvf Cython-0.21.1.tar.gz
@@ -21,5 +21,5 @@ python setup.py install --prefix=${HOME}
 cd ..
 wget https://github.com/scikit-image/scikit-image/zipball/master
 unzip master
-cd scikit-image-scikit-image-0325d9f/
+cd scikit-image-scikit-image-*/
 python setup.py install --prefix=${HOME}
